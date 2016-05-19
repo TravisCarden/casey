7366641e9888e38848af60aceeff975fe2322b0f
TravisCarden <travis.carden@acquia.com>
2016-05-18 23:32:08 -0500

    Added a failing test for incipient Casey class with toCamelCase() method.

--- tests/src/Unit/CaseyTest.php
+++ tests/src/Unit/CaseyTest.php
@@ -2,8 +2,18 @@
 
 namespace Drupal\Tests\casey\Unit;
 
+use Drupal\casey\Casey;
 use Drupal\Tests\UnitTestCase;
 
 class CaseyTest extends UnitTestCase {
+
+  public function testToCamelCase() {
+    // Arrange.
+    $casey = new Casey();
+    // Act.
+    $output = $casey->toCamelCase('');
+    // Assert.
+    $this->assertSame('', $output, 'Empty string');
+  }
 
 }
